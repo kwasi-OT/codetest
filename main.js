0c9710236d64905349509c65a33d647eb0ade9c2
Kwasi O-T <kwasi@kudigo.com>
2023-12-13 17:28:51 +0000

    adjusted styling and re-organized logic

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 let selectedRating;
 
-  // Function to handle button clicks
-function handleButtonClick(event) {
-const rating = event.target.value;
-if (rating) {
-    selectedRating = rating;
-    document.querySelector('.rating').innerText = selectedRating;
-}
-}
+document.getElementById('rating').style.display = 'flex';
+document.getElementById('thankYou').style.display = 'none';
 
+  // Function to handle rating button clicks
+function handleButtonClick(event) {
+    const rating = event.target.value;
+    if (rating) {
+        selectedRating = rating;
+        document.querySelector('.rating').innerText = selectedRating;
+    }
+}
+// function to handle submit button and event
 function handleSubmit() {
-    document.getElementById('ratingCard').style.display = 'none';
-    document.getElementById('thankYou').style.display = 'block';
+    document.getElementById('rating').style.display = 'none';
+    document.getElementById('thankYou').style.display = 'flex';
 }
 
 // Add click event listeners to rating buttons
